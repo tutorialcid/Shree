16e1592948bb5863dea94d138aa6c8c9b9e25cfe
tutorialcid <adaniblog852@gmail.com>
2017-03-15 10:21:25 -0700

    Create team.js

---
+++ Adn Safelink Template v.2.1 Black/team.js
@@ -0,0 +1,27 @@
+ href='ULR BLOG SOBAT' rel='canonical'/>
+<link href='ULR BLOG SOBAT' hreflang='id' rel='alternate'/>
+<link href='http://www.blogger.com/openid-server.g' rel='openid.server'/>
+<meta content='ULR ALAMAT ANDA' property='og:url'/>
+<meta content='NAMA BLOG ANDA' property='og:site_name'/>
+<meta content='Indonesia' name='geo.placename'/>
+<meta content='NAMA ANDA' name='Author'/>
+<meta content='general' name='rating'/>
+<meta content='all' name='audience'/>
+<meta content='id' name='geo.country'/>
+<meta content='NOODP' name='robots'/>
+<meta content='INDEX,FOLLOW' name='Robots'/>
+<!-- Noindex halaman pencarian, label, dan arsip -->
+<!-- Meta keywords otomatis halaman depan, statis, dan postingan -->
+<meta content='MAsukan keywords' name='keywords'/>
+<meta content='Masukan ogdescription' property='og:description'/>
+<meta content='http://www.tutorialblogcom.com/favicon.ico' property='og:image'/>
+<meta content='KODE VERIFIKASI webmaster BING' name='msvalidate.01'/>
+<meta content='KODE VERIVIKASI ALEXA' name='alexaVerifyID'/>
+  <link href='ULR BLOG ANDA/page-1' rel='canonical'/>
+<link href='ULR BLOG ANDA/feeds/posts/default' rel='alternate' title='ADANIZONE - Atom' type='application/atom+xml'/>
+<link href='ULR BLOG ANDA/feeds/posts/default?alt=rss' rel='alternate' title='TUTORIALBLOB - RSS' type='application/rss+xml'/>
+<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700|Bitter:400,700,400italic' rel='stylesheet' type='text/css'/>
+<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css'/>
+<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet'/>
+<link href='https://www.blogger.com/dyn-css/authorization.css?targetBlogID=MASUKAN ID BLOG SOBAT&amp;zx=9ec5d135-8cc9-4a36-adf1-40aad0c2bd9a' rel='stylesheet' type='text/css'/>
+<script>
